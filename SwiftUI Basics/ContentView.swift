925a4385c81a1ede622dafbc505415b139f22c34
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-06 16:19:07 +0300

    MultilineTextAlignment, Rotations in 3D

--- SwiftUI Basics/ContentView.swift
+++ SwiftUI Basics/ContentView.swift
@@ -6,11 +6,19 @@ struct ContentView: View {
     let colorList2: [Color] = [.black, .white, .blue]
     
     @State private var screenTapped: Bool = false
-    @State private var offsetY: CGFloat = -1000.0
+    let rotationAngle: CGFloat = 360.0
     
     // Computed property
     var colors: [Color] {
         screenTapped ? colorList1 : colorList2
+    }
+    
+    var text: String {
+        screenTapped ? "SwiftUI Basics" : "To know what you know and what you do not know, that is true knowledge"
+    }
+    
+    var font: Font {
+        screenTapped ?  .largeTitle : .title
     }
     
     var body: some View {
@@ -22,16 +30,28 @@ struct ContentView: View {
             )
             .opacity(0.8)
                 .ignoresSafeArea()
-            Text("SwiftUI Basics 1")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .foregroundStyle(.black)
-                .offset(y: offsetY)
+            VStack {
+                Text(text)
+                    .font(font)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.red)
+                    .multilineTextAlignment(.center)
+                    .frame(maxHeight: .infinity)
+                
+                Text("Another line of text")
+                    .font(font)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(screenTapped ? .blue : .indigo)
+                    .multilineTextAlignment(.center)
+                    .frame(maxHeight: .infinity)
+            }
+            .padding(.horizontal)
+            .rotation3DEffect(.degrees(screenTapped ? 0 : rotationAngle), axis: (x:0, y:1, z:0))
+            
         }
         .onTapGesture {
             withAnimation(.easeIn(duration: 0.5)) {
                 screenTapped.toggle()
-                offsetY = screenTapped ? 0 : -1000
             }
         }
     }
