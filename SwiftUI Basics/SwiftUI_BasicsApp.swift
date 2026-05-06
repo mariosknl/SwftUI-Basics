8976a7f0351629772fe4b1fe0ba456e44d8d6520
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-06 15:42:50 +0300

    Initial Commit

---
+++ SwiftUI Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI Basics
+//
+//  Created by Marios Kanellopoulos on 6/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
