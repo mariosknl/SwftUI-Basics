fc6714b504dceeae1b7b690a7640c484de96b47b
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-07 09:23:11 +0300

    Arithmetic Progression

---
+++ SwiftUI Basics/ArithmeticeProgression.swift
@@ -0,0 +1,18 @@
+//
+//  ArithmeticeProgression.swift
+//  SwiftUI Basics
+//
+//  Created by Marios Kanellopoulos on 7/5/26.
+//
+
+import SwiftUI
+
+struct ArithmeticeProgression: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArithmeticeProgression()
+}
